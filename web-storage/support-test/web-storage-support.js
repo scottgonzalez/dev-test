999aef110ebe06648a6a4a61fcf9c627fd978cb0
Scott González <scott.gonzalez@gmail.com>
2011-05-29 17:39:13 -0400

    Web storage: Fixed clearing of sessionStorage in old versions of Firefox.

--- web-storage/support-test/web-storage-support.js
+++ web-storage/support-test/web-storage-support.js
@@ -28,26 +28,53 @@ var storage = {
 		}
 	},
 
-	sessionStorage: {
-		setup: function( done ) {
-			try {
-				if ( !sessionStorage.getItem ) {
+	sessionStorage: (function() {
+		// support for old versions of Firefox that don't have sessionStorage.clear()
+		function clear( done ) {
+			function iterate() {
+				try {
+					// throws when there are no keys
+					var key = sessionStorage.key( 0 );
+					sessionStorage.removeItem( key );
+				} catch( error ) {
+					return done();
+				}
+				setTimeout( iterate, 1 );
+			}
+			iterate();
+		}
+
+		return {
+			setup: function( done ) {
+				try {
+					if ( !sessionStorage.getItem ) {
+						return done( false );
+					}
+				} catch ( e ) {
 					return done( false );
 				}
-			} catch ( e ) {
-				return done( false );
+				try {
+					sessionStorage.clear();
+					return done( true );
+				} catch( error ) {
+					clear(function() {
+						done( true );
+					});
+				}
+			},
+			run: function( key, value ) {
+				sessionStorage.setItem( key, value );
+			},
+			teardown: function( done ) {
+				try {
+					sessionStorage.clear();
+					return done();
+				} catch( error ) {
+					clear( done );
+				}
 			}
-			sessionStorage.clear();
-			return done( true );
-		},
-		run: function( key, value ) {
-			sessionStorage.setItem( key, value );
-		},
-		teardown: function( done ) {
-			sessionStorage.clear();
-			done();
 		}
-	},
+	}()),
 
 	globalStorage: (function() {
 		function clear( done ) {
